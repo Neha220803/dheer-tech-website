7374a3cc4af6bda9edaf1ca719c5c6d7026267ce
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-09-06 20:55:03 +0530

    home page changes

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 
+const headerVid = "/assets/videos/earth-hero-vid.mp4";
+
 export const HomeHeroComp = () => {
   return (
-    <section className="bg-primary ">
-      <div className="grid md:grid-cols-2 grid-cols-1 h-screen">
-        <div className="flex justify-center items-center">Image/Video</div>
-        <div className="flex flex-col justify-center gap-4">
-          <div className="w-3/4 flex flex-col justify-center gap-4">
+    <div className="bg-black text-white h-screen">
+      <div className="grid md:grid-cols-2 grid-cols-1 h-screen mx-30">
+        <div className="flex justify-center items-center p-4">
+          <video
+            src={headerVid}
+            autoPlay
+            // loop
+            // muted
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center gap-4 ">
+          <div className="w-3/4  flex flex-col justify-center gap-4">
             <h1 className="text-4xl font-medium">Secure. Scalable. Smart</h1>
             <p className="text-lg">
               We design and develop web-based solutions that empower businesses
@@ -15,6 +25,6 @@ export const HomeHeroComp = () => {
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
