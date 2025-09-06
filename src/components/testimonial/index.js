9dfd35bf01d8a5e5643ae1f4baad6bd6977cd30c
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-09-06 20:14:02 +0530

    testimonial page done

--- src/components/testimonial/index.js
+++ src/components/testimonial/index.js
@@ -1,9 +1,14 @@
-import React from "react";
+import "./styles.css";
+import CardsScrollComp from "./CardsScroll";
 
 const HomeTestimonialComp = () => {
   return (
-    <section>
-      <h1 className="text-4xl">Testimonial page</h1>
+    <section className="testimonial-section ">
+      {/* <div className="testimonial-header  items-center justify-between mb-8 px-10">
+        <h2 className="text-3xl font-semibold">What Our Users Say</h2>
+      </div> */}
+      {/* Scrollable Row */}
+      <CardsScrollComp />
     </section>
   );
 };
